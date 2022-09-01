980f97097970f64495a256adc44714984d6e823c
mmexploit <mmexploit1@gmail.com>
2022-09-01 17:29:40 +0300

    added sign-in componenet with styling modified header styles

---
+++ src/components/pages/sign-in-and-sign-up/sign-ins-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './sign-in-and-sign-up.styles.css'
+import SignIn from '../../sign-in/sign-in.component';
+
+const SignInAndSignUp = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn/>
+    </div>
+)
+
+export default SignInAndSignUp;
