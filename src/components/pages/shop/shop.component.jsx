e9bbf726af2183ca96bed01e85817a9b129d90b2
mmexploit <mmexploit1@gmail.com>
2022-09-14 13:03:45 +0300

    moved shop data to firestore database and made the shop fetch the items from it

--- src/components/pages/shop/shop.component.jsx
+++ src/components/pages/shop/shop.component.jsx
@@ -3,14 +3,32 @@ import { Route } from 'react-router-dom'
 
 import CollectionOverview from '../../../components/collection-overview/collection-overview.component'
 import CollectionPage from '../collection/collection.component'
+
+
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../../firebase/firebase.utils'
 import './shop-page.styles.css'
 
 
-const ShopPage = ({ match }) => (
+class ShopPage extends React.Component {
+    unsubscribeFromSnapshot = null;
+
+    componentDidMount () {
+        const collectionRef = firestore.collection("collections")
+
+        collectionRef.onSnapshot(async snapshot => {
+            convertCollectionsSnapshotToMap(snapshot);
+        })
+    }
+   render() { 
+    const { match } = this.props;
+   return ( 
     <div className="shop-page">
-       <Route exact path={`${match.path}`} component={CollectionOverview}/>
-       <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
-    </div>
-)
+        <Route exact path={`${match.path}`} component={CollectionOverview}/>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+        </div>
+    )
+   }
+} 
 
 export default ShopPage
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -43,6 +43,34 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
 firebase.initializeApp(config)
 
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    console.log(collectionRef)
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj)
+    })
+
+    return await batch.commit()
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        };
+    })
+
+    console.log(transformedCollection)
+} 
+
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
 
