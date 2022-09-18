b9e5db182bf89465e881dfdee9ce5a6f3545eac4
mmexploit <mmexploit1@gmail.com>
2022-09-18 11:21:25 +0300

    fixed the error that comes when a specific page of shop is refereshed by changing the way the app checks whether data is loaded or not, instead of isFetching used another selector with sleectIsCollectionLoaded functionality

--- src/components/pages/shop/shop.component.jsx
+++ src/components/pages/shop/shop.component.jsx
@@ -1,55 +1,48 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import {connect} from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import CollectionOverview from '../../../components/collection-overview/collection-overview.component'
 import CollectionPage from '../collection/collection.component'
 
-import { updateCollections } from '../../../redux/shop/shop.actions'
+import { fetchCollectionsStartAsync } from '../../../redux/shop/shop.actions'
+import { selectIsCollectionFetching, selectIsCollectionLoaded } from '../../../redux/shop/shop.selectors'
 
 import WithSpinner from '../../with-spinner/with-spinner.component'
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../../firebase/firebase.utils'
+
 import './shop-page.styles.css'
 
 const CollectionOverviewWithSpinnner = WithSpinner(CollectionOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-    state = {
-        loading: true
-    }
-
     unsubscribeFromSnapshot = null;
 
     componentDidMount () {
-        const { updateCollections } = this.props;
-
-        const collectionRef = firestore.collection("collections");
-
-
-        collectionRef.get().then(snapshot => {
-            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-            updateCollections(collectionsMap);
-            this.setState({ loading: false });
-        })
+        const { fetchCollectionsStartAsync } = this.props;
+        fetchCollectionsStartAsync();
  
     }
    render() { 
-    const { match } = this.props;
-    const { loading } = this.state;
+    const { match, isCollectionFetching, isCollectionsLoaded } = this.props;
     return ( 
             <div className="shop-page">
-                <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinnner isLoading={loading} {...props}/>}/>
-                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props}/>}/>
+                <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinnner isLoading={!isCollectionsLoaded} {...props}/>}/>
+                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props}/>}/>
             </div>
     )
    }
 } 
 
-const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => 
-        dispatch(updateCollections(collectionsMap))
+const mapStateToProps = createStructuredSelector({
+    isCollectionFetching: selectIsCollectionFetching,
+    isCollectionsLoaded: selectIsCollectionLoaded,
 })
 
-export default connect(null, mapDispatchToProps)(ShopPage)
+const mapDispatchToProps = dispatch => ({
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage)
